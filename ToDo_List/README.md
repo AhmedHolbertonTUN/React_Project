73fed2de416fb565e350656cc3020b2ddb24befb
AhmedHolbertonTUN <jarrayaahmed119@gmail.com>
2020-08-20 13:42:38 +0100

    ToDo list

---
+++ ToDo_List/README.md
@@ -0,0 +1,8 @@
+﻿# TODO list React Hooks
+
+**OBJECTIVES**  
+
+You should be able to:  
+- Add a new task by clicking to the ADD button  
+- View the list of the tasks  
+- Delete and edit tasks  
---
+++ ToDo_List/src/App.js
@@ -0,0 +1,55 @@
+import React,{useState} from "react";
+import "./styles.css";
+
+export default function App() {
+  const [newtodo,setNewtodo]= useState("");
+  const [tasks,setTasks] = useState([]);
+
+  function handleNewtodoChange(e){
+    e.preventDefault()
+    setNewtodo(e.target.value)    
+  };
+
+  function handleNewTask(e){
+    e.preventDefault();
+    if (newtodo === "") return
+    setTasks([...tasks, {id: Date.now(), text: newtodo}])
+    e.target.reset()
+    setNewtodo("")
+  };
+  
+  function TaskRemove(id){
+    setTasks(tasks.filter((task)=> task.id !== id))
+  }
+
+  function TaskEdit(id,todo){
+  let index;
+  let text=prompt("Edit the task");
+  for(let i=0;i<tasks.length;i++)
+  if (tasks[i].id===id)
+  index=i;
+  if (text !== "")
+  tasks.splice(index, 1, {id:id , text: text});
+  setTasks(tasks.filter((task)=> task.test !== todo))
+  }
+  
+  return (
+    <div className="App">
+      <h1>My ToDo List</h1>
+<form className='head' onSubmit={handleNewTask}>
+  <input type="text" placeholder="Add to your list..." onChange={handleNewtodoChange} />
+  <input type="submit" value="ADD" />
+</form>
+  <ul>
+   {tasks.map((el)=>(
+   <li key={el.id}><div>{el.text}</div>
+   <div>
+   <button className='button' onClick={()=>TaskRemove(el.id)}>Delete</button>
+   <button className='button' onClick={()=>TaskEdit(el.id,el.text)}>Edit</button>
+   </div>
+   </li>
+   ))}
+  </ul>
+</div>
+  );
+}
